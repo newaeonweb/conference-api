f73bd8e7da71288a26fd9f0bbecbbb2a335ebe0a
fernando monteiro <fernando@newaeonweb.com.br>
2014-07-04 22:15:24 -0300

    second refactoring

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@
 module.exports = {
     // Uncomment to connect with MongoDB on Cloud
      'url' : 'mongodb://feiochc:hate666!@kahana.mongohq.com:10073/node-api'
-	//'url' : 'localhost/easy'
+	//'url' : 'localhost/conferenceDB'
 };
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,18 +1,12 @@
 // load all the things we need
 var LocalStrategy    = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy  = require('passport-twitter').Strategy;
-var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy;
 
 // load up the user model
 var User       = require('../../server/models/user');
 
-// load the auth variables
-var configAuth = require('./auth'); // use this one for testing
-
 module.exports = function(passport) {
-
     // passport setup
+
     // serialize the user for the session
     passport.serializeUser(function(user, done) {
         done(null, user.id);
@@ -34,24 +28,24 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
         if (email)
-            // force lower-case
+            // format to lower-case
             email = email.toLowerCase();
 
         // asynchronous
         process.nextTick(function() {
             User.findOne({ 'local.email' :  email }, function(err, user) {
-                // if there are any errors
+                // if errors
                 if (err)
                     return done(err);
 
-                // if no user is found, return the message
+                // check errors and bring the messages
                 if (!user)
                     return done(null, false, req.flash('loginMessage', 'No user found.'));
 
                 if (!user.validPassword(password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+                    return done(null, false, req.flash('loginMessage', 'Wohh! Wrong password.'));
 
-                // all is well, return user
+                // everything ok, get user
                 else
                     return done(null, user);
             });
@@ -68,7 +62,7 @@ module.exports = function(passport) {
     },
     function(req, email, password, done) {
         if (email)
-            // force lower-case
+            // format to lower-case
             email = email.toLowerCase();
 
         // asynchronous
@@ -76,19 +70,19 @@ module.exports = function(passport) {
             // if the user is not already logged in:
             if (!req.user) {
                 User.findOne({ 'local.email' :  email }, function(err, user) {
-                    // if there are any errors
+                    // if errors
                     if (err)
                         return done(err);
 
-                    // check to see if theres already a user with that email
+                    // check email
                     if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                        return done(null, false, req.flash('signupMessage', 'Wohh! the email is already taken.'));
                     } else {
 
                         // create the user
-                        var newUser            = new User();
+                        var newUser = new User();
 
-                        newUser.local.email    = email;
+                        newUser.local.email = email;
                         newUser.local.password = newUser.generateHash(password);
 
                         newUser.save(function(err) {
@@ -100,19 +94,9 @@ module.exports = function(passport) {
                     }
 
                 });
-            // if the user is logged in but has no local account...
-            } else if ( !req.user.local.email ) {
-                // ...presumably they're trying to connect a local account
-                var user            = req.user;
-                user.local.email    = email;
-                user.local.password = user.generateHash(password);
-                user.save(function(err) {
-                    if (err)
-                        throw err;
-                    return done(null, user);
-                });
+
             } else {
-                // user is logged in and already has a local account. Ignore signup. (You should log out before trying to create a new account, user!)
+
                 return done(null, req.user);
             }
 
